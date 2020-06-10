6f92d9076f4f102600ecb8b2331bf7c2e96e9ca6
john_imgrund <john.imgrund@mymail.champlain.edu>
2020-06-10 00:15:58 -0400

    Set up the main shooting system for the player. right now the bullet class is agnostic, but the basicBullet is player specifc. Most likely subclass bullets will have to be specific to either player or enemy. Also tried to future proof the system for more ammo types later on, should make adding new ammo types a breeze.

---
+++ BulletHell/Assets/Scripts/bullets/basicBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicBullet : Bullet
+{
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.velocity = transform.up * Speed; //Set the bullet in motion
+
+        //Destroy it after duration.
+        Destroy(gameObject, duration);
+    }
+}
---
+++ BulletHell/Assets/Scripts/shootPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootPlayer : MonoBehaviour
+{
+    //public variables
+    //--------------------
+    public Transform bulletSpawn;
+    public Bullet[] bulletTypes;
+    //--------------------
+
+    //private variables
+    //--------------------
+    int currentBulletType;
+    float timer;
+    //--------------------
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentBulletType = 0; //always start with basic bullet
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= (1.0f / bulletTypes[currentBulletType].FiringRate)) //Firing rate is based in bullets per second
+        {
+            FireBullet();
+
+            timer = 0;
+        }
+    }
+
+    //Fire current ammo type
+    void FireBullet()
+    {
+
+        Instantiate((GameObject)Resources.Load("Prefabs/" + bulletTypes[currentBulletType].BulletName), bulletSpawn.position, transform.rotation);
+    }
+
+    //Switch ammo type
+    void SetCurrentBulletType(int value)
+    {
+        currentBulletType = value;
+    }
+}
